df6fc46289c18ead1a4a0d506764d0b67ba7388a
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-06-07 22:11:32 -0700

    add urls and views for account app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('This is home page.')
+
+def products(request):
+    return HttpResponse('This is products page.')
+
+def customer(request):
+    return HttpResponse('This is customer page.')
