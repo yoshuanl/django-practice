f4aa33422f33785b81fc4bf5a95627b216e3e906
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-06-08 11:07:22 -0700

    add database models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+# inherit from django models
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Many-to-Many relationship
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+    name = models.CharField(max_length=200, null=True)
+    tags = models.ManyToManyField(Tag, max_length=200, null=True)
+    price = models.FloatField(max_length=200, null=True)
+    category = models.CharField(max_length=200, choices=CATEGORY, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+    customer = models.ForeignKey(Customer, max_length=200, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, max_length=200, null=True, on_delete=models.SET_NULL)
+
+    status = models.CharField(max_length=200, choices=STATUS, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
